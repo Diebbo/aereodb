b60bd78c740c5f5046b3a36040dcc35679232377
Ivan <ivan03ds@gmail.com>
2025-01-10 16:56:24 +0100

    inizio ui operazioni

--- nextapp/app/page.tsx
+++ nextapp/app/page.tsx
@@ -1,6 +1,20 @@
-import Image from "next/image";
+import Image from "next/image"
+import clsx from 'clsx'
+import { 
+  Form,
+  Button,
+  Select,
+  SelectItem
+} from '@nextui-org/react'
 
 export default function Home() {
+  const operations = [
+    { key: 'c', label: 'Inserimento'},
+    { key: 'r', label: 'Ricerca'},
+    { key: 'u', label: 'Modifica'},
+    { key: 'd', label: 'Eliminazione'},
+  ]
+
   return (
     <div>
       <header>
@@ -12,9 +26,18 @@ export default function Home() {
         <Image
           className="rounded-lg"
           src="/wtc.jpg"
-          alt="Airplanes image"
+          alt="Immagine di aereo ed edifici"
           width={525}
           height={295} />
+        <Form className="flex flex-row items-center">
+          <Select label="Seleziona l'operazione">
+            {operations.map((o) => (
+              <SelectItem key={o.key}>{o.label}</SelectItem>
+            ))}
+          </Select>
+          <Button type='reset'>Reset</Button>
+          <Button type="submit">Esegui</Button>
+        </Form>
       </main>
     </div>
   );
