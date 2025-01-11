c2815b13c4f075406835e26bd47e651aadd82279
Ivan240103 <ivan03ds@gmail.com>
2025-01-11 17:35:54 +0100

    ci vuole tanta pazienza

--- nextapp/app/components/retrieve.tsx
+++ nextapp/app/components/retrieve.tsx
@@ -1,14 +1,41 @@
+'use client'
+
 import React from "react";
+import query from "../lib/db";
+import { QueryResult } from "mysql2";
 
 type RetrieveProps = {
   e: string
 };
 
-const Retrieve: React.FC<RetrieveProps> = ({ e }) => {
+const Retrieve: React.FC<RetrieveProps> = async ({ e }) => {
+  const [result, setResult] = React.useState<QueryResult>()
+
+  const excQuery = async () => {
+    switch (e) {
+      case 'r-partenze':
+        setResult(await query('SELECT * FROM aeroporto AS a, volo AS v WHERE a.IATA = v.IATAPartenza AND a.ICAO = v.ICAOPartenza ORDER BY a.nome'))
+        break
+      case 'r-arrivi':
+        setResult(await query('SELECT * FROM aeroporto AS a, volo AS v WHERE a.IATA = v.IATAArrivo AND a.ICAO = v.ICAOArrivo ORDER BY a.nome'))
+        break
+      case 'r-lav-aeroporto':
+      case 'r-lav-comp-aeree':
+      case 'r-lav-comp-log':
+      case 'r-passeggeri':
+      case 'r-bagagli':
+      case 'r-merci':
+        setResult(await query('SELECT * FROM pacco'))
+        break
+      case 'r-serv-aeroporto':
+      case 'r-serv-sicurezza':
+      case 'r-serv-trasporto':
+      case 'r-parcheggi':
+    }
+  }
   return (
-    <div className="p-4 border rounded-lg shadow-lg">
-      <h2 className="text-xl font-semibold">Valore di e:</h2>
-      <p className="text-gray-700">{e}</p>
+    <div onLoad={excQuery}>
+      <p>{e}</p>
     </div>
   );
 };
