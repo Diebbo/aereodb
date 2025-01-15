b4d46be6c8192403620eeeae33aff83598b3b38c
Ivan <ivan03ds@gmail.com>
2025-01-15 12:18:57 +0100

    finish refactor

--- nextapp/app/components/retrieve.tsx
+++ nextapp/app/components/retrieve.tsx
@@ -2,23 +2,36 @@
 
 import { useState, useEffect } from "react";
 import { retrieve, selectAllFrom } from "../actions/query";
-import { Button, Form, Select, SelectItem } from "@nextui-org/react";
+import {
+  Alert,
+  Button,
+  Form,
+  Select,
+  SelectItem
+} from "@nextui-org/react";
 
 export default function Retrieve({ q }: { q: string }) {
-  const [result, setResult] = useState<any>(undefined)
-  const [error, setError] = useState<any>('')
+  const [error, setError] = useState<string>('')
+  // dati da visualizzare
+  const [data, setData] = useState<any>(undefined)
+  // elenco degli aeroporti
   const [airports, setAirports] = useState<any>([])
+  // aeroporto selezionato
   const [selectedAirport, setSelectedAirport] = useState<string>('')
 
+  // prende l'elenco degli aeroporti se serve
   useEffect(() => {
     const fetchAirports = async () => {
-      if (needsPK()) {
+      if (needsAirport()) {
         try {
-          const data = await selectAllFrom('aeroporto')
+          setAirports(await selectAllFrom('aeroporto'))
           setError('')
-          setAirports(data)
         } catch (error) {
-          setError(error)
+          console.error(error)
+          setError('fetchAirports error')
+          setTimeout(() => {
+            setError('')
+          }, 4000);
         }
       }
     }
@@ -31,7 +44,7 @@ export default function Retrieve({ q }: { q: string }) {
    * 
    * @returns true se serve la ricerca aggiuntiva
    */
-  const needsPK = () => {
+  const needsAirport = () => {
     return(
       q == 'r-partenze' ||
       q == 'r-arrivi' ||
@@ -41,68 +54,88 @@ export default function Retrieve({ q }: { q: string }) {
     )
   }
 
+  /**
+   * Esegue la query di ricerca
+   */
   const fetchDb = async () => {
     try {
-      const data = needsPK() ? await retrieve(q, selectedAirport.split(',')) : await retrieve(q, [])
+      if (needsAirport()) {
+        setData(await retrieve(q, selectedAirport.split(',')))
+      } else {
+        setData(await retrieve(q, []))
+      }
       setError('')
-      setResult(data)
     } catch (error) {
-      setError(error)
+      console.error(error)
+      setError('Errore nella ricerca')
     }
   }
 
   return (
     <div>
-      {error && <span className="error">error</span>}
-      <div className="flex flex-col items-center gap-4 w-full max-w-md">
-        <Form
-          className="flex flex-col items-center gap-4 w-full max-w-md"
-          validationBehavior="native"
-          onSubmit={(e) => {
-            e.preventDefault()
-            fetchDb()
-          }}
-        >
-          {needsPK() && <Select
-            className="flex-1 min-w-36"
-            label="Aeroporto"
-            isRequired
-            selectedKeys={[selectedAirport]}
-            onChange={e => setSelectedAirport(e.target.value)}
+      {error ? (
+        <div key='danger' className="w-full flex items-center my-3 fade-in">
+          <Alert color='danger' variant="solid" title={error} />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-4 w-full max-w-md fade-in">
+          <Form
+            className="flex flex-col items-center gap-4 w-full max-w-md"
+            validationBehavior="native"
+            onSubmit={(e) => {
+              e.preventDefault()
+              fetchDb()
+            }}
           >
-            {airports.map((a: any) => (
-              <SelectItem key={a.IATA + ',' + a.ICAO} textValue={`${a.IATA} - ${a.ICAO}`}>
-                {a.IATA} - {a.ICAO}
-              </SelectItem>
-            ))}
+            {needsAirport() && <Select
+              className="flex-1 min-w-36"
+              label="Aeroporto"
+              isRequired
+              selectedKeys={[selectedAirport]}
+              onChange={e => setSelectedAirport(e.target.value)}
+            >
+              {airports.map((a: any) => (
+                <SelectItem
+                  key={a.IATA + ',' + a.ICAO}
+                  textValue={`${a.IATA} - ${a.ICAO}`}
+                >
+                  {a.IATA} - {a.ICAO}
+                </SelectItem>
+              ))}
             </Select>}
-          <Button type='submit' color="primary">Esegui ricerca</Button>
-        </Form>
-        {result ? (<div>
-          {result.length === 0 ? (
-            <p>Nessun risultato</p>
-          ) : (
-            <table className="border-1 border-black border-collapse">
-              <thead>
-                <tr>
-                  {Object.keys(result[0]).map((key: string) => (
-                    <th key={key} className="border-1 border-black p-5">{key}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {result.map((row: any, i: number) => (
-                  <tr key={i}>
-                    {Object.values(row).map((value: any, j: number) => (
-                      <td key={j} className="border-1 border-black align-center p-5">{`${value}`}</td>
+            <Button type='submit' color="primary">Esegui ricerca</Button>
+          </Form>
+
+          {data ? (
+            <div>
+              {data.length === 0 ? (
+                <div className="my-16">
+                  <p>Nessun risultato</p>
+                </div>
+              ) : (
+                <table className="border-1 border-black border-collapse">
+                  <thead>
+                    <tr>
+                      {Object.keys(data[0]).map((key: string) => (
+                        <th key={key} className="border-1 border-black p-5">{key}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((row: any, i: number) => (
+                      <tr key={i}>
+                        {Object.values(row).map((value: any, j: number) => (
+                          <td key={j} className="border-1 border-black align-center p-5">{`${value}`}</td>
+                        ))}
+                      </tr>
                     ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>) : null}
-      </div>
+                  </tbody>
+                </table>
+              )}
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 };
--- nextapp/app/components/table.tsx
+++ nextapp/app/components/table.tsx
@@ -2,22 +2,27 @@
 
 import { useState, useEffect } from "react";
 import { selectAllFrom } from "../actions/query";
+import {
+  Alert
+} from '@nextui-org/react'
 
 export default function Table({ q }: { q: string }) {
-  const [error, setError] = useState<any>('')
+  const [error, setError] = useState<string>('')
+  // dati da visualizzare
   const [data, setData] = useState<any>(undefined)
 
-  console.log('TABLE - q:', q)
-
+  // prende i dati della tabella richiesta
   useEffect(() => {
     const fetchDb = async () => {
       try {
-        const result = await selectAllFrom(q.split('-')[1])
+        setData(await selectAllFrom(q.split('-')[1]))
         setError('')
-        setData(result)
       } catch (error) {
-        setError(error)
         console.error(error)
+        setError('Errore durante il fetch')
+        setTimeout(() => {
+          setError('')
+        }, 4000);
       }
     }
 
@@ -26,31 +31,42 @@ export default function Table({ q }: { q: string }) {
 
   return (
     <div>
-      {error && <span className="error">Error</span>}
-      {data ? (<div>
-        {data.length === 0 ? (
-          <p>Nessun risultato</p>
-        ) : (
-          <table className="border-1 border-black border-collapse">
-            <thead>
-              <tr>
-                {Object.keys(data[0]).map((key: string) => (
-                  <th key={key} className="border-1 border-black p-5">{key}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((row: any, i: number) => (
-                <tr key={i}>
-                  {Object.values(row).map((value: any, j: number) => (
-                    <td key={j} className="border-1 border-black align-center p-5">{`${value}`}</td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>) : null}
+      {error ? (
+        <div key='danger' className="w-full flex items-center my-3 fade-in">
+          <Alert color='danger' variant="solid" title={error} />
+        </div>
+      ) : (
+        <div className="fade-in">
+          {data ? (
+            <div>
+              {data.length === 0 ? (
+                <div className="my-16">
+                  <p>Nessun risultato</p>
+                </div>
+              ) : (
+                <table className="border-1 border-black border-collapse">
+                  <thead>
+                    <tr>
+                      {Object.keys(data[0]).map((key: string) => (
+                        <th key={key} className="border-1 border-black p-5">{key}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((row: any, i: number) => (
+                      <tr key={i}>
+                        {Object.values(row).map((value: any, j: number) => (
+                          <td key={j} className="border-1 border-black align-center p-5">{`${value}`}</td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 };
